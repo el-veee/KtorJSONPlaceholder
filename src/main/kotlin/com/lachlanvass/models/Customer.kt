e928ee7f66588fd4d44650708b91f4475fa05b53
lachlanvass <lachlan.vass.dev@gmail.com>
2022-08-21 19:19:43 +1000

    feat: add Customer test data

--- src/main/kotlin/com/lachlanvass/models/Customer.kt
+++ src/main/kotlin/com/lachlanvass/models/Customer.kt
@@ -5,13 +5,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
 
-val customerStorage = (1..900000).map { it -> generateCustomer(it) }
-
-fun generateCustomer(id: Int): Customer {
-    return Customer(
-        id = id.toString(),
+val customerStorage = mutableListOf(
+    Customer(
+        id = "1",
         firstName = "Random",
         lastName = "Random2",
         email = "Random.Random@mail.com"
     )
-}
+)
